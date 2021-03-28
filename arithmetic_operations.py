28c9fa144546ac3976690069a0aa713e08de92f8
eyaya <eyayab21@gmail.com>
2021-03-28 22:50:51 +0300

    Add files via upload

---
+++ arithmetic_operations.py
@@ -0,0 +1,39 @@
+
+def add(a,b):
+	"""
+	This fucntion adds two numbers and return the result
+	"""
+	c = a+b
+	return c
+	
+def subtract(a,b):
+	"""
+	This fucntion substracts two numbers and return the result
+	"""
+	c = a-b
+	return c
+	
+def multiply(a,b):
+	"""
+	This fucntion multiplies two numbers and return the result
+	"""
+	c = a*b
+	return c
+	
+def divide(a,b):
+	"""
+	This fucntion divides two numbers and return the result
+	"""
+	if b==0:
+		return("division by zero is not allowed")
+	else:
+		c = a/b
+		return c
+	
+def absolute(a):
+	"""
+	A fucntion that computes the absolute value of a number
+	"""
+	if a <0:
+		a *=-1
+	return a
